100dd1459cf83b098ba515475b4d3c85668aa5d1
Sooraj Janardhanan <soorajkj46@gmail.com>
2025-10-26 19:40:09 +0530

    fix: :art: ppdate forcedTheme to return undefined for non light paths

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider as NextThemeProvider } from "next-themes";
 import { usePathname } from "next/navigation";
 
 function forcedTheme(pathname: string) {
-  return ["/", "/about"].includes(pathname) ? "light" : "dark";
+  return ["/", "/about"].includes(pathname) ? "light" : undefined;
 }
 
 export default function ThemeProvider({ ...props }: ThemeProviderProps) {
